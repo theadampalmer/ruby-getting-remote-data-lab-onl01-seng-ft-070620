e1ec92240952ca5f123e103b4263666c11d6729a
Adam Palmer <theadampalmer@gmail.com>
2020-07-25 21:45:15 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,13 +8,14 @@ class GetRequester
         @url = url
     end
 
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def parse_json
-    JSON.parse(get_response_body)
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
     end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+    
 end 
